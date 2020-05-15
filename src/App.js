c5509a033f84ae487c5efabf60a767844dedbc68
Reza Karimi <rezakarimi@google.com>
2020-05-15 19:16:44 +0000

    setting up trace

--- src/App.js
+++ src/App.js
@@ -7,43 +7,51 @@ import { tracing } from '@opencensus/web-instrumentation-zone';
 
 require('dotenv').config()
 
+
+
 function App() {
     const [vendors, setVendors] = useState([]);
     const [suppliers, setSuppliers] = useState([]);
 
     useEffect(() => {
-        var res = [];
-        var promises = [];
-        console.log(suppliers);
-        for (var i = 0; i < suppliers.length; i++) {
-            console.log(process.env.REACT_APP_VENDOR + suppliers[i]);
-            promises.push(axios.get(process.env.REACT_APP_VENDOR + suppliers[i], {
-                params: {
-                    ingredient: document.getElementById("searchbar").value
-                }
-            }).then(response => {
-                console.log("data", response)
-                if (Object.keys(response.data).length > 1){
-                    res.push(response.data);
-                }
-            }));
-        }
-        axios.all(promises).then((results) => {
-            setVendors(res);
-            console.log(res);
+        console.log(tracing.tracer.currentRootSpan)
+        tracing.tracer.startRootSpan({ name: 'getting vendors', samplingRate: 1.0 }, async rootSpan => {
+            var res = [];
+            var promises = [];
+            console.log(suppliers);
+            const vendorsCallSpan = tracing.tracer.startChildSpan({ name: 'getting data from vendors' });
+            for (var i = 0; i < suppliers.length; i++) {
+                console.log(process.env.REACT_APP_VENDOR + suppliers[i]);
+                promises.push(axios.get(process.env.REACT_APP_VENDOR + suppliers[i], {
+                    params: {
+                        ingredient: document.getElementById("searchbar").value
+                    }
+                }).then(response => {
+                    console.log("data", response)
+                    if (Object.keys(response.data).length > 1) {
+                        res.push(response.data);
+                    }
+                }));
+            }
+            axios.all(promises).then((results) => {
+                vendorsCallSpan.end();
+                setVendors(res);
+                console.log(res);
+            });
+            rootSpan.end();
         });
     }, [suppliers]);
 
     const handleSubmit = (event) => {
-        event.preventDefault()
-        console.log(document.getElementById("searchbar").value)
-        const apiCallSpan = tracing.tracer.startChildSpan({ name: 'api call latency' });
+        event.preventDefault();
+        console.log(document.getElementById("searchbar").value);
+        const supplierCallSpan = tracing.tracer.startChildSpan({ name: 'retrieving possible vendors from the supplier server' });
         axios.get(process.env.REACT_APP_SUPPLIER, {
             params: {
                 ingredient: document.getElementById("searchbar").value
             }
         }).then(response => {
-            apiCallSpan.end()
+            supplierCallSpan.end();
             setSuppliers(response.data);
         });
     }
@@ -56,19 +64,19 @@ function App() {
                         <Form.Control id="searchbar" type="text" placeholder="Search for an ingredient i.e. yeast, flour" />
                     </Col>
                     <Col xs={1}>
-                        <Button variant="primary" type="submit">
+                        <Button data-ocweb-id='Trace user interaction' id="searchBtn" onClick={handleSubmit} type="submit">
                             Search
                         </Button>
                     </Col>
                 </Row>
             </Form>
             <Row style={{ paddingTop: "50px" }}>
-                {vendors.map((product, index) => <Col xs={4}  key={index}>
+                {vendors.map((product, index) => <Col xs={4} key={index}>
                     <Card>
                         <Card.Body>
                             <Card.Title>{product.vendor}</Card.Title>
                             <Card.Text>
-                                Quantity: {product.quantity}<br/>
+                                Quantity: {product.quantity}<br />
                                 Price: {product.price}
                             </Card.Text>
                         </Card.Body>
